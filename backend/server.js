d2bd69e9d7e4738252c24fade8d8fed625a2f10a
Umapathi2004 <umapathi1014@gmail.com>
2024-07-29 21:41:05 +0530

    changed

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const http = require("http");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+const app = express();
+const PORT = process.env.port || 3000;
+
+app = http.createServer((req,res)=>{
+    res.write("hi");
+    res.end();
+})
+
+app.listen(PORT, ()=>(console.log(`The port is: ${PORT}`)));
